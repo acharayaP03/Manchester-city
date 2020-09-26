e9ce0aa4c34eea82f3a3c8bd029eaa9f0464a201
acharayaP03 <acharyaP03@gmail.com>
2020-09-26 16:00:31 +1000

    Complete Hero section

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Layouts from './Hocs/Layouts';
+import {Switch, Route} from 'react-router-dom';
+
+import Home from  './Components/Home'
 
 const Routes = (props) => {
     return (
         <Layouts>
-            second line
+            <Switch>
+                <Route exact component={Home} path="/"/>
+            </Switch>
         </Layouts>
     )
 }
